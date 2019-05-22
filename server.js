5f9c3325b28cdc90b53d65cc645ee96b32686553
Jasmine <jasminerojas102@gmail.com>
2019-05-22 17:02:36 -0400

    adding mocha

--- server.js
+++ server.js
@@ -2,12 +2,16 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-
+const {app, runServer, closeServer} = require('../server');
 
 const app = express();
+const expect = chai.expect;
+chai.use(chaiHttp);
 
 app.use('/', express.static('public'))
 app.use(morgan('common'));
